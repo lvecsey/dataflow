faa40c0f0be1942fa4e26a33616294d6eae2df01
Lester Vecsey <lvecsey@gmail.com>
2018-02-21 01:12:55 +0000

    add hostport.h

---
+++ hostport.h
@@ -0,0 +1,13 @@
+#ifndef HOSTPORT_H
+#define HOSTPORT_H
+
+#include <stdint.h>
+
+typedef struct __attribute__ ((packed)) {
+
+  uint8_t ip_address[4];
+  uint16_t port;
+  
+} hostport_t;
+
+#endif
