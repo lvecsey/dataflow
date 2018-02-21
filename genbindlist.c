3170d6cbfe8ce4b38e87ea5255a82a365806c5ce
Lester Vecsey <lvecsey@gmail.com>
2018-02-21 16:04:56 +0000

    added genbindlist.

---
+++ genbindlist.c
@@ -0,0 +1,71 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <string.h>
+#include <errno.h>
+#include <stdint.h>
+
+#include <arpa/inet.h>
+
+#include "hostport.h"
+
+#include "rep.h"
+
+int main(int argc, char *argv[]) {
+
+  char *input_fn = argc>1 ? argv[1] : NULL;
+  
+  FILE *fp = input_fn != NULL ? fopen(input_fn,"r") : stdin;
+
+  char *line = NULL;
+  
+  size_t len = 0;
+
+  long int lineno = 0;
+
+  int retval;
+
+  hostport_t hp;
+
+  int out_fd = 1;
+  ssize_t bytes_written;
+
+  uint16_t rep;
+
+  char *host_extract;
+  
+  while ((retval=getline(&line,&len,fp)) != -1) {
+
+    if (len > 0) {
+
+      lineno++;
+
+      host_extract = line;
+      
+      retval = fill_hostport(&hp, host_extract);
+      if (retval != 0) {
+	fprintf(stderr, "%s: Expected to extract target host and parsing failed.\n", __FUNCTION__);
+	return -1;
+      }
+
+      bytes_written = write(out_fd, &hp, sizeof(hostport_t));
+      if (bytes_written != sizeof(hostport_t)) {
+	perror("write");
+	return -1;
+      }
+      
+    }
+
+  }
+
+  fclose(fp);
+  
+  return 0;
+
+}
